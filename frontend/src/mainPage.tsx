13a67bc8ebd6cb7ce925c891512dddb662218345
Aleksey Nizhgorodov <8900800700a@gmail.com>
2024-06-09 04:02:42 +0300

    Update mainPage.tsx

--- frontend/src/mainPage.tsx
+++ frontend/src/mainPage.tsx
@@ -1,5 +1,20 @@
-const MainPage = () => {
-  return <div>asd</div>;
+import { Tooltip } from "components/tooltip.tsx";
+import { Text } from "components/text.ts";
+import { Stack } from "components/stack.ts";
+
+export const MainPage = () => {
+  return (
+    <Stack direction={"column"} gap={20} align={"center"}>
+      <Tooltip content="ого тултип (некрасывый работает)" action={"hover"}>
+        <Text size={16}>тест тултипа</Text>
+      </Tooltip>
+      <Text>
+        В чем прикол переделанного роутинга, мы можешь иметь общий лейаут для
+        всех страниц. И так можно
+        делать с любыми вложенными роутами
+      </Text>
+    </Stack>
+  );
 };
 
 export default MainPage;
