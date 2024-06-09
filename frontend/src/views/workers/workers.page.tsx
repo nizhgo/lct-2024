fbedb8ae24335bbea453525a7d7bafc0b7f20077
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-06-09 12:51:17 +0300

    start workers page

--- frontend/src/views/workers/workers.page.tsx
+++ frontend/src/views/workers/workers.page.tsx
@@ -1,5 +1,25 @@
+import { Text } from "components/text.ts";
+import styled from "@emotion/styled";
+import { Button } from "components/button.tsx";
+import { useNavigate } from "react-router-dom";
+
+const ContentHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const WorkersPage = () => {
-  return <div>workers</div>;
+  const navigate = useNavigate();
+  const handleAdd = () => {
+    navigate("/worker/registration"); // Перенаправление после успешного входа
+  };
+
+  return (
+    <ContentHeader>
+      <Text size={32}>Сотрудники</Text>
+      <Button onClick={handleAdd}>Добавить сотрудника</Button>
+    </ContentHeader>
+  );
 };
 
 export default WorkersPage;
