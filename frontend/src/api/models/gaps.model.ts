efaebc393cafaa63da90ff7a334caa4c78760785
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-06-22 02:48:11 +0300

    create gaps form

--- frontend/src/api/models/gaps.model.ts
+++ frontend/src/api/models/gaps.model.ts
@@ -2,13 +2,29 @@ import { z } from "zod";
 import { UsersDto } from "api/models/users.model.ts";
 
 export namespace GapsDto {
+  export const Status = z.enum([
+    "Рабочий",
+    "Не полный рабочий",
+    "Выходной",
+    "Больничный",
+    "Отпускной",
+  ]);
+
+  export type Status = z.infer<typeof GapsDto.Status>;
+  export const statusValues: Status[] = [
+    "Рабочий",
+    "Не полный рабочий",
+    "Выходной",
+    "Больничный",
+    "Отпускной",
+  ];
   export const Gap = z.object({
     id: z.number(),
     user_id: z.number(),
     start_time: z.string(),
     end_time: z.string(),
     is_working: z.boolean(),
-    status: z.string(),
+    status: Status,
     description: z.string(),
     is_deleted: z.boolean(),
     user: UsersDto.User,
